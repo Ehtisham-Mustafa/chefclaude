d260f002a26afdca6b879ea288b4f4f03bbc8087
Ehtisham_Mustafa <ehtishammustafa0@gmail.com>
2025-03-05 21:29:28 +0500

    on submit event

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 export default function Main(){
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    const ingredientListItems=ingredients.map(ingredeient=>(
+        <li key={ingredeient}>{ingredeient}</li>
+
+    ));
+    function handlesubmit(e){
+        e.preventDefault()
+        const formData=new FormData(e.currentTarget)
+        const newIngredient=formData.get("ingredient")
+        ingredients.push(newIngredient)
+        console.log(ingredients)
+    }
     return(
         <main>
-        <form className="add-ingredient-form">
-            <input type="text" placeholder="e.g. oregano" aria-label="Add ingredient"/>
+        <form onSubmit={handlesubmit} className="add-ingredient-form">
+            <input type="text" name="ingredient" placeholder="e.g. oregano" aria-label="Add ingredient"/>
             <button>Add Ingredient</button>
         </form>
+        <ul>
+                {ingredientListItems}
+        </ul>
         </main>
     )
 }
