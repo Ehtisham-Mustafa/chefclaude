1a6a73cf8076ce5db813dcef3d9aff78db2d682c
Ehtisham_Mustafa <ehtishammustafa0@gmail.com>
2025-03-06 15:26:03 +0500

    add apple

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 export default function Main(){
-    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    const ingredients = ["Chicken", "Oregano", "Tomatoes","Apple"]
     const ingredientListItems=ingredients.map(ingredeient=>(
         <li key={ingredeient}>{ingredeient}</li>
 
@@ -15,7 +15,7 @@ export default function Main(){
     return(
         <main>
         <form onSubmit={handlesubmit} className="add-ingredient-form">
-            <input type="text" name="ingredient" placeholder="e.g. oregano" aria-label="Add ingredient"/>
+            <input type="text" name="ingredient" placeholder="e.g. oranges" aria-label="Add ingredient"/>
             <button>Add Ingredient</button>
         </form>
         <ul>
