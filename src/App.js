09ce5ce0dd09e1a354572119634c6846b7a077b8
Ehtisham_Mustafa <ehtishammustafa0@gmail.com>
2025-03-19 21:45:51 +0500

    sounds pad code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
 import Header from './Components/Header';
 import Main from './Components/Main';
+import Pads from './Components/Pads';
+import padsData from './DataSource/pads';
+import React from 'react';
 
 function App() {
+  const [pads,setPads]=React.useState(padsData)
+
+  function toggle(id){
+    setPads(prevpads=>prevpads.map(item=>{
+    return item.id === id ? {...item,on: !item.on}:item
+    }
+    ))
+  }
+  const buttonElements=pads.map(pad=>(
+    <Pads key={pad.id} id={pad.id} color={pad.color} on={pad.on} toggle={toggle} />    
+))
   return (
-    <>
-      <Header/>
-      <Main/>
-    </>
+    <main>
+      {/* <Header/>
+      <Main/> */}
+
+      <div className="pad-container">
+      {buttonElements}
+        </div>
+     
+    </main>
   );
 }
 
---
+++ src/Components/Pads.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Pad(props) {
+    const [on, setOn] = React.useState(props.on)
+    
+    function toggle() {
+        setOn(prevOn => !prevOn)
+    }
+
+    return (
+        <button 
+            style={{backgroundColor: props.color}}
+            className={props.on ? "on" : undefined}
+            onClick={()=>props.toggle(props.id)}
+        ></button>
+    )
+}
