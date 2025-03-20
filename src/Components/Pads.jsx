7fdfc52fe5dec4123a9e2a17d7d35046fbc68fa4
Ehtisham_Mustafa <ehtishammustafa0@gmail.com>
2025-03-20 15:43:16 +0500

    daily progress

--- src/Components/Pads.jsx
+++ src/Components/Pads.jsx
@@ -10,7 +10,7 @@ export default function Pad(props) {
     return (
         <button 
             style={{backgroundColor: props.color}}
-            className={props.on ? "on" : null}
+            className={props.on ? "on" : undefined}
             onClick={()=>props.toggle(props.id)}
         ></button>
     )
