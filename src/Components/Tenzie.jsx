f4deb1e3d8f5ec0fbb2946ef59bfc89c140bc37f
Ehtisham_Mustafa <ehtishammustafa0@gmail.com>
2025-03-21 21:40:37 +0500

    first project done

--- src/Components/Tenzie.jsx
+++ src/Components/Tenzie.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useRef,useEffect } from 'react'
 import Die from './Die'
 import { nanoid } from 'nanoid'
 import Confetti from 'react-confetti'
@@ -7,9 +7,13 @@ export default function Tenzie(){
 
     const [dice,setDice]=React.useState(()=>generateAllNewDice())
     const [tenzies,setTenzies]=useState(false)
-
+    const buttonRef = useRef(null)
     const gameWon=dice.every(die=>die.isHeld) && dice.every(die=>die.value===dice[0].value)
-
+    useEffect(() => {
+        if (gameWon) {
+            buttonRef.current.focus()
+        }
+    }, [gameWon])
     function generateAllNewDice(){
       return new Array(10)
       .fill(0)
@@ -54,7 +58,7 @@ export default function Tenzie(){
             <div className="dice-container">
       {diceElements}
          </div>
-         <button className='roll-dice' onClick={rollDice}>{gameWon?"New Game":"Roll"}</button>
+         <button ref={buttonRef} className='roll-dice' onClick={rollDice}>{gameWon?"New Game":"Roll"}</button>
         </main>
     )
 }
